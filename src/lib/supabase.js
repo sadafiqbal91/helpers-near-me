ab0b9a5e4aba87d16db5ca03a029e898241bd173
sadafiqbal91 <imsadafiqbal91@gmail.com>
2026-04-14 19:47:58 +0500

    final fix

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,10 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-// Supabase project URL
 const supabaseUrl = "https://mxwiawnjpciagwiyseat.supabase.co";
-
-// ✅ Publishable (Anon) Key
 const supabaseAnonKey = "sb_publishable__Wd0J_rKiTOqecAVoXPJCA_f8KZN3p0";
 
-// Create Supabase client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
